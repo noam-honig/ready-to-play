28fadb8515fe854d549e9e34e06227eee44fec71
Noam Honig <noam.honig@gmail.com>
2024-05-01 15:20:37 +0300

    Updated ready to play

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,10 +1,18 @@
 import { remultExpress } from 'remult/remult-express'
 import { Task } from '../shared/model.js'
+import sqlite3 from 'sqlite3'
+import { Sqlite3DataProvider } from 'remult/remult-sqlite3'
+import { SqlDatabase } from 'remult'
 
 export const entities = [Task]
 
 export const api = remultExpress({
   entities,
   admin: true,
+  // dataProvider: new SqlDatabase(
+  //   // Note that on stackblitz, this database name is only saved per user on their own browser.
+  //   // To store the database on stackblitz, change the name not to include a dot on the start.
+  //   new Sqlite3DataProvider(new sqlite3.Database('.database.sqlite'))
+  // ),
   getUser: (req) => req.session!['user'],
 })
